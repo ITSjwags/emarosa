28b4700a1bed184d1625e36304b478b32dd43f0c
ITSjwags <jvwagoner@gmail.com>
2018-11-13 18:03:17 -0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'emarosa',
+        short_name: 'emarosa',
         start_url: '/',
-        background_color: '#663399',
+        background_color: '#000000',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
