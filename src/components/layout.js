4f336ad6279e5d450f601b5665684ee0b040541b
ITSjwags <jvwagoner@gmail.com>
2018-11-13 17:13:46 -0800

    Change layout to class to hook into lifecycle event

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import styled, { createGlobalStyle } from 'styled-components';
@@ -37,46 +37,50 @@ const Logo = styled.img`
   max-width: 250px;
 `;
 
-const Layout = () => {
-  // make sure height is 100vh even in mobile safari
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+class Layout extends Component {
+  componentDidMount() {
+    // make sure height is 100vh even in mobile safari
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 
-  return (
-    <StaticQuery
-      query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-      render={data => (
-        <>
-          <Normalize />
-          <GlobalStyle />
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Emarosa' },
-              { name: 'keywords', content: 'emarosa, peach club, band, music, rock, pop' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
+        `}
+        render={data => (
+          <>
+            <Normalize />
+            <GlobalStyle />
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Emarosa' },
+                { name: 'keywords', content: 'emarosa, peach club, band, music, rock, pop' },
+              ]}
+            >
+              <html lang="en" />
+            </Helmet>
 
-          <Content>
-            <Logo src={LogoSrc} alt="Emarosa" />
-            <p>11•15•18</p>
-          </Content>
-        </>
-      )}
-    />
-  );
+            <Content>
+              <Logo src={LogoSrc} alt="Emarosa" />
+              <p>11•15•18</p>
+            </Content>
+          </>
+        )}
+      />
+    );
+  }
 }
 
 Layout.propTypes = {
