16d70559b4ce867d7aaac8806781694a966d0776
ITSjwags <jvwagoner@gmail.com>
2018-11-14 16:30:43 -0800

    Add in height tweaks for mobile

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import styled, { createGlobalStyle } from 'styled-components';
+import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import styled, { createGlobalStyle } from 'styled-components';
 import { Normalize } from 'styled-normalize';
+import debounce from 'lodash.debounce';
 import Background from './background';
 import Header from './header';
 
@@ -31,49 +32,83 @@ const BackgroundContainer = styled.div`
   width: 100vw;
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  min-height: calc(var(--vh, 1vh) * 100);
+`;
+
 const Content = styled.main`
-  padding: 0 20px 40px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  padding: 0 20px 40px 20px;
   text-align: center;
 `;
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+class Layout extends Component {
+  componentDidMount() {
+    this.getHeight();
+    window.addEventListener('resize', debounce(this.getHeight, 200));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.getHeight);
+  }
+
+  getHeight = () => {
+    // make sure height is 100vh even in mobile safari
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <StaticQuery
+        query={graphql`
+          query SiteTitleQuery {
+            site {
+              siteMetadata {
+                title
+              }
+            }
           }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Normalize />
-        <GlobalStyle />
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Emarosa' },
-            { name: 'keywords', content: 'emarosa, peach club, band, music, rock, pop' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
+        `}
+        render={data => (
+          <>
+            <Normalize />
+            <GlobalStyle />
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Emarosa' },
+                { name: 'keywords', content: 'emarosa, peach club, band, music, rock, pop' },
+              ]}
+            >
+              <html lang="en" />
+            </Helmet>
 
-        <BackgroundContainer>
-          <Background />
-        </BackgroundContainer>
+            <BackgroundContainer>
+              <Background />
+            </BackgroundContainer>
 
-        <Header />
+            <Wrapper>
+              <Header />
 
-        <Content>
-          {children}
-        </Content>
-      </>
-    )}
-  />
-);
+              <Content>
+                {children}
+              </Content>
+            </Wrapper>
+          </>
+        )}
+      />
+    );
+  }
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
