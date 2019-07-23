48fc06364d8323786ecf517e146401d56152bd9e
ITSjwags <jvwagoner@gmail.com>
2019-07-23 08:00:01 -0700

    Update splash page to show new tour poster

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,95 +1,55 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import styled from 'styled-components';
+
 import Layout from '../components/layout';
+
+import posterSrc from '../images/tour-poster-web2.jpg';
+
+const IndexPage = () => (
+  <Layout>
+    <Helmet>
+      <script
+        charset="utf-8"
+        src="https://widget.bandsintown.com/main.min.js"
+      />
+    </Helmet>
+    <Content>
+      <img src={posterSrc} alt="Emarosa Peach Club Tour" />
+      <a
+        className="bit-widget-initializer"
+        data-artist-name="Emarosa"
+        data-display-local-dates="true"
+        data-display-past-dates="false"
+        data-auto-style="false"
+        data-text-color="#FFFFFF"
+        data-link-color="#E63167"
+        data-background-color="rgba(0,0,0,0)"
+        data-display-limit="15"
+        data-display-start-time="true"
+        data-link-text-color="#FFFFFF"
+        data-display-lineup="true"
+        data-display-play-my-city="true"
+        data-separator-color="rgba(124,124,124,0.25)"
+        href="#asf"
+      >
+        &nbsp;
+      </a>
+    </Content>
+  </Layout>
+);
 
 const Content = styled.div`
   margin: 0 auto;
   position: relative;
   width: 100%;
-`;
+  max-width: 960px;
 
-const Videos = styled.div`
-  display: grid;
-  grid-gap: 40px;
-  grid-template-rows: repeat(2, 1fr);
-  justify-items: center;
-
-  @media(min-width: 960px) {
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: initial;
-  }
-`;
-
-const Video = styled.div`
-  height: 0;
-  overflow: hidden;
-  padding-bottom: 56.25%;
-  position: relative;
-  width: 100%;
-
-  > iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
+  > img {
+    display: block;
+    margin-bottom: 10px;
     width: 100%;
   }
 `;
-
-const Link = styled.a`
-  background: white;
-  border-radius: 30px;
-  border: 2px solid white;
-  color: #ca4766;
-  display: inline-block;
-  font-style: italic;
-  font-weight: bold;
-  margin-top: 40px;
-  padding: 15px 30px;
-  text-decoration: none;
-  text-transform: uppercase;
-  transition: all 250ms ease;
-
-  &:hover {
-    background: #ca4766;
-    border-color: white;
-    color: white;
-  }
-`;
-
-const IndexPage = () => (
-  <Layout>
-    <Content>
-      <Videos>
-        <Video>
-          <iframe
-            width="560"
-            height="315"
-            title="peach club - givin up video"
-            src="https://www.youtube.com/embed/Shmt5YnF40A?modestbranding=1"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Video>
-        <Video>
-          <iframe
-            width="560"
-            height="315"
-            title="131 - cloud 9 video"
-            src="https://www.youtube.com/embed/pVxCOWYcmFk?modestbranding=1"
-            frameBorder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        </Video>
-      </Videos>
-
-      <Link href="http://smarturl.it/emarosamerch" target="_blank">
-        Shop Merch
-      </Link>
-    </Content>
-  </Layout>
-);
 
 export default IndexPage;
