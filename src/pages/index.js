c53d0daae5a684e2bb4f1926942eae6b8c1b9538
ITSjwags <jvwagoner@gmail.com>
2019-11-17 20:01:14 -0800

    Fix import case

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet';
 import styled from 'styled-components';
 
 import Layout from '../components/layout';
-import Logo from '../components/Logo';
+import Logo from '../components/logo';
 
 const IndexPage = () => (
   <Layout>
